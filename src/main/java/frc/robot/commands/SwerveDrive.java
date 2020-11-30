4361ff8252cfe9502043ab02ec80e23bd34e3a74
O0f <34110946+O0f@users.noreply.github.com>
2020-11-29 19:49:50 -0500

    Syncing code with Chris

--- src/main/java/frc/robot/commands/SwerveDrive.java
+++ src/main/java/frc/robot/commands/SwerveDrive.java
@@ -1,6 +1,8 @@
 package frc.robot.commands;
 
 import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import frc.robot.subsystems.Drivetrain;
@@ -11,16 +13,23 @@ public class SwerveDrive extends CommandBase {
   private DoubleSupplier transMag;
   private DoubleSupplier transAngle;
 
-
   public SwerveDrive(DoubleSupplier rotX, DoubleSupplier transMag, DoubleSupplier transAngle) {
     this.rotX = rotX;
     this.transMag = transMag;
     this.transAngle = transAngle;
     addRequirements(m_Drivetrain);
+    SmartDashboard.putNumber("transMag Const", this.transMag.getAsDouble());
   }
 
   @Override
   public void execute() {
     m_Drivetrain.drive(this.rotX.getAsDouble(), this.transMag.getAsDouble(), this.transAngle.getAsDouble());
+    SmartDashboard.putNumber("transMag Exe", this.transMag.getAsDouble());
   }
+
+  @Override
+  public boolean isFinished() { return false; }
+
+  @Override
+  public void end(boolean interrupted) {  }
 }
